2db63b4674844f520f32d48f8f530bf527d44b30
Rick <Work@Richards-MacBook-Pro-2.local>
2018-06-06 13:49:32 -0700

    Changed approach, from insertion into an ID3 tag => APE tag

--- Methods.cs
+++ Methods.cs
@@ -12,40 +12,55 @@ namespace AddToMp3
 {
     public class Methods
     {
-
+        //Creates and saves a .txt ByteFile file, and outputs to the console.
         public static void ReadBytes(byte[] file, Encoding FileEncoding){
             Console.OutputEncoding = FileEncoding; //set the console output.
-            string ByteFile = Hex.Dump(file); //Store 
-            System.IO.File.WriteAllText("./testDoneSecond.txt", ByteFile); //Create a document containing Mp3 ByteFile (debugging).
+            string ByteFile = Hex.Dump(file); //Store Hex the Hex data from the Mp3 file.
+            System.IO.File.WriteAllText("./Mp3HexPrintout.txt", ByteFile); //Create a document containing Mp3 ByteFile (debugging).
             Console.WriteLine(ByteFile); //Log
         }
-
-        public static void CreateAFrame(TagLib.File Mp3File, TagLib.Id3v2.Tag Mp3Tag, byte[] MyCloudCoin, Encoding FileEncoding)
+        public static void CreateAnApeFrame(TagLib.Ape.Tag ApeTag, string MyCloudCoin, Encoding FileEncoding)
         {
-             // Note that the third parameter is true while writing.
-            PrivateFrame PrivFram = PrivateFrame.Get(Mp3Tag, "CloudCoins", true); //Create a new ID3 frame of type private. 
-            PrivFram.PrivateData = MyCloudCoin; //Insert the CloudCoin into the new frame
-            Console.WriteLine("PrivFrame created: " + PrivFram); //Log 
-            Console.WriteLine("PrivFrame data: " + PrivFram.PrivateData); //Log
-        }
-
-        public static void ReadAFrame(TagLib.File Mp3File, TagLib.Id3v2.Tag Mp3Tag, Encoding FileEncoding)
-        {
-            // Retreive the ID3TAG.
-            // Note that the third parameter is false while reading.
-            PrivateFrame PrivFrame = PrivateFrame.Get(Mp3Tag, "CloudCoins", false); //Read a ID3 frame of type private.
-            //Retrieve the CloudCoin from the PRIV frame.
-            //Note without a PRIV frame will result in error. needs error check.
-            string CloudCoin = FileEncoding.GetString(PrivFrame.PrivateData.Data); //Store the CloudCoins in CloudCoin variable. 
-            System.IO.File.WriteAllText("./CloudCoin.stack.txt", CloudCoin); //Create and copy the CloudCoins to a new file.
-            Console.WriteLine("Reading PrivFrame: " + CloudCoin); //Log
+            if(ApeTag.IsEmpty){
+                TagLib.Ape.Tag CloudCoinApeTag = ApeTag; //Create a new Apev2 frame. 
+                CloudCoinApeTag.SetValue("CloudCoinArea_", MyCloudCoin); //Insert the CloudCoin into the new frame
+                Console.WriteLine("ApeFrame created: " + CloudCoinApeTag); //Log 
+                Console.WriteLine("ApeFrame data: " + CloudCoinApeTag.GetItem("CloudCoins")); //Log
+            }
 
         }
+        public static void CheckApeTag(TagLib.Ape.Tag ApeTag){
+            if (ApeTag != null) { // Check for an ApeTag 
+                Console.WriteLine("ApeTag exists"  ); //Log 
+            }
+        }
+        public static void SetApeTagValue(TagLib.File Mp3File, TagLib.Ape.Tag ApeTag, string MyCloudCoin){
+            // Get the APEv2 tag if it exists.
+            if(ApeTag != null) {
+                ApeTag.SetValue("CloudCoinArea_", MyCloudCoin);
+            }
+        }
+        public static void ReturnCloudCoins(TagLib.Ape.Tag ApeTag){
+            TagLib.Ape.Item item = ApeTag.GetItem("CloudCoinArea_");
+                if (item != null) {
+                    string CloudCoinAreaValues = item.ToString();
+                    using (FileStream fs = System.IO.File.Create("./CloudCoinPrintout.json"))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes(CloudCoinAreaValues);
+                        // Add some information to the file.
+                        fs.Write(info, 0, info.Length);
+                    }
+                    Console.WriteLine(item);
+                }else{
+                    Console.WriteLine("no CloudCoins");
+                }
+        }
 
 
 
 
 
+// Removed Code
 
         //     //CheckEncoding
         //     //Used for debugging.
@@ -64,6 +79,31 @@ namespace AddToMp3
         //     return Encoding.ASCII;
         // }
 
+
+        //Used with Id3 tags
+        // public static void ReadAFrame(TagLib.File Mp3File, TagLib.Id3v2.Tag Mp3Tag, Encoding FileEncoding)
+        // {
+        //     // Retreive the ID3TAG.
+        //     // Note that the third parameter is false while reading.
+        //     PrivateFrame PrivFrame = PrivateFrame.Get(Mp3Tag, "CloudCoins", false); //Read a ID3 frame of type private.
+        //     //Retrieve the CloudCoin from the PRIV frame.
+        //     //Note without a PRIV frame will result in error. needs error check.
+        //     string CloudCoin = FileEncoding.GetString(PrivFrame.PrivateData.Data); //Store the CloudCoins in CloudCoin variable. 
+        //     System.IO.File.WriteAllText("./CloudCoin.stack.txt", CloudCoin); //Create and copy the CloudCoins to a new file.
+        //     Console.WriteLine("Reading PrivFrame: " + CloudCoin); //Log
+
+        // }
+
+
+        //Used to create private frames for id3 tags
+        //         public static void CreateAnId3Frame(TagLib.File Mp3File, TagLib.Id3v2.Tag Mp3Tag, string MyCloudCoin, Encoding FileEncoding)
+        // {
+        //      // Note that the third parameter is true while writing.
+        //     PrivateFrame PrivFram = PrivateFrame.Get(Mp3Tag, "CloudCoins", true); //Create a new ID3 frame of type private. 
+        //     PrivFram.PrivateData = MyCloudCoin; //Insert the CloudCoin into the new frame
+        //     Console.WriteLine("PrivFrame created: " + PrivFram); //Log 
+        //     Console.WriteLine("PrivFrame data: " + PrivFram.PrivateData); //Log
+        // }
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -16,24 +16,35 @@ namespace AddToMp3
         {
             //Path pointing to the MP3 & CloudCoin files.
             string Mp3Path = "./pew.mp3";
-            string CCPath = "./test.txt";
+            string CCPath = "./CloudCoin.json";
             Encoding FileEncoding = Encoding.ASCII;
 
             //Key passed into 
-            byte[] MyCloudCoin = System.IO.File.ReadAllBytes(CCPath);
-            byte[] MyMp3 = System.IO.File.ReadAllBytes(Mp3Path);
+            string MyCloudCoin = System.IO.File.ReadAllText(CCPath);
+
             TagLib.File Mp3File = TagLib.File.Create(Mp3Path);
 
             // You can add a true parameter to the GetTag function if the Mp3File doesn't already have a Mp3Tag.
-            // By passing a the parameter TagTypes.Id3V2 we ensure the type is of Id3v2.
-            TagLib.Id3v2.Tag Mp3Tag = (TagLib.Id3v2.Tag)Mp3File.GetTag(TagTypes.Id3v2);
-            
-            Methods.CreateAFrame(Mp3File, Mp3Tag, MyCloudCoin, FileEncoding); // Create private frame.
-            Methods.ReadAFrame(Mp3File, Mp3Tag, FileEncoding); // Read contents of private frame.
-            Methods.ReadBytes(MyMp3, FileEncoding);
-            
+            // By passing a the parameter TagTypes.Ape we ensure the type is of Ape.
+
+            TagLib.Ape.Tag ApeTag = (TagLib.Ape.Tag)Mp3File.GetTag(TagLib.TagTypes.Ape, true);
+
+
+            // Methods.CreateAnApeFrame(ApeTag, MyCloudCoin, FileEncoding); // Create an Ape frame
+            Methods.CheckApeTag(ApeTag);
+            Methods.SetApeTagValue(Mp3File, ApeTag, MyCloudCoin);
             Mp3File.Save(); // Save changes.
+
+            byte[] MyMp3 = System.IO.File.ReadAllBytes(Mp3Path);
+            Methods.ReadBytes(MyMp3, FileEncoding);
+            Methods.ReturnCloudCoins(ApeTag);
+            
         }
     }
 }
+
+//Removed code
+            // TagLib.Id3v2.Tag Mp3Tag = (TagLib.Id3v2.Tag)Mp3File.GetTag(TagTypes.Id3v2);
+            // Methods.CreateAnId3Frame(Mp3File, Mp3Tag, MyCloudCoin, FileEncoding); // Create private frame.
+            // Methods.ReadAFrame(Mp3File, Mp3Tag, FileEncoding); // Read contents of private frame.
 
